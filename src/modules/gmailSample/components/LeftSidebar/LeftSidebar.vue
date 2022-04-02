9b6e52f6e93eae160c19845f9155a745db770f2b
Fabio Duarte <mail.fabio.duarte@gmail.com>
2022-04-01 19:08:01 -0500

    feat: The script to toggle the color on compose button on hover state is now into 'LeftSidebar.vue' and was removed from the 'index.html' and the whole project as well as the directory 'js'

--- src/modules/gmailSample/components/LeftSidebar/LeftSidebar.vue
+++ src/modules/gmailSample/components/LeftSidebar/LeftSidebar.vue
@@ -302,6 +302,19 @@
 
 
 <script>
+
+const compose = document.querySelector('#compose');
+
+compose.addEventListener('mouseenter', () => {
+    compose.parentElement.classList.toggle('hover:bg-[#414141]')
+});
+
+compose.addEventListener('mouseleave', () => {
+    compose.parentElement.classList.toggle('hover:bg-[#414141]')
+    console.log('salio');
+})
+
+
     export default {
         name: 'LeftSidebar'
     }
