4e224a962f185705c4f2aa892e1dc14b91f129ff
Fabio Duarte <mail.fabio.duarte@gmail.com>
2022-05-09 12:01:10 -0500

    refactor: All the old code that originally built the project on 'index.html' removed; and the script to make the visual effect on 'Compose.vue' was moved to 'index.html' under a <script> tag (this still doesn't works as it should)

--- src/modules/gmailSample/components/layouts/LeftSidebar.vue
+++ src/modules/gmailSample/components/layouts/LeftSidebar.vue
@@ -13,17 +13,6 @@
 
 <script>
 import { defineAsyncComponent } from "vue"
-const compose = document.querySelector('#compose');
-
-compose.addEventListener('mouseenter', () => {
-    compose.parentElement.classList.toggle('hover:bg-[#414141]')
-});
-
-compose.addEventListener('mouseleave', () => {
-    compose.parentElement.classList.toggle('hover:bg-[#414141]')
-    console.log('salio');
-})
-
 
     export default {
         name: 'LeftSidebar',
